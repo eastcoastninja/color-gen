46f92521bebca99dca4fb93d093fd66e4dc0fc38
Julian Rios <julianrios.dev@gmail.com>
2018-08-05 20:37:20 -0400

    beautify webpages with color

---
+++ script.js
@@ -0,0 +1,51 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var random = document.querySelector(".btn");
+
+setGradient();
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function getRandomRgb() {
+	  var num = Math.round(0xffffff * Math.random());
+	  var r = num >> 16;
+	  var g = num >> 8 & 255;
+	  var b = num & 255;
+	  return 'rgb(' + r + ', ' + g + ', ' + b + ')';	
+}
+
+function getRandomHex() {
+	  var letters = '0123456789ABCDEF'.split('');
+      var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.round(Math.random() * 15)];
+        }
+        return color;
+}
+
+function randomColorGen() {
+	color1.value = getRandomHex();
+	color2.value = getRandomHex();
+
+	body.style.background = "linear-gradient(to right, " 
+	+ color1.value + ", " + color2.value; + ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+random.addEventListener("click", randomColorGen);
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
