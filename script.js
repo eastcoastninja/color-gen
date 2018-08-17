ed5cb1d3c104488c6cb918a309c07e5e95a1cdb1
Julian Rios <julianrios.dev@gmail.com>
2018-08-16 21:40:05 -0400

    update to es6 syntax and restructured code

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-var _ = require('lodash');
+
+// lodash npm package
+let _ = require('lodash');
 
 console.log(_);
-var array = [1,2,3,4,5,6,7,8];
+let array = [1,2,3,4,5,6,7,8];
 console.log('answer:', _.without(array, 3));
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var random = document.querySelector(".btn");
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let random = document.querySelector(".btn");
 
 setGradient();
 
-function setGradient() {
+const setGradient = () => {
 	body.style.background = 
 	"linear-gradient(to right, " 
 	+ color1.value 
 	+ ", " 
 	+ color2.value 
 	+ ")";
-
+	
 	css.textContent = body.style.background + ";";
 }
 
-function getRandomRgb() {
-	  var num = Math.round(0xffffff * Math.random());
-	  var r = num >> 16;
-	  var g = num >> 8 & 255;
-	  var b = num & 255;
+const getRandomRgb() => {
+	  let num = Math.round(0xffffff * Math.random());
+	  let r = num >> 16;
+	  let g = num >> 8 & 255;
+	  let b = num & 255;
 	  return 'rgb(' + r + ', ' + g + ', ' + b + ')';	
 }
 
-function getRandomHex() {
-	  var letters = '0123456789ABCDEF'.split('');
-      var color = '#';
-        for (var i = 0; i < 6; i++ ) {
+const getRandomHex = () => {
+	  let letters = '0123456789ABCDEF'.split('');
+      let color = '#';
+        for (let i = 0; i < 6; i++ ) {
             color += letters[Math.round(Math.random() * 15)];
         }
         return color;
 }
 
-function randomColorGen() {
+const randomColorGen = () => {
 	color1.value = getRandomHex();
 	color2.value = getRandomHex();
 
